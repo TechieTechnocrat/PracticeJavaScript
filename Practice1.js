8f9bb70976ed301b7507ca1b07c8751b67a78628
sonal1729 <sonal.kumar2019@vitbhopal.ac.in>
2023-07-02 23:44:01 +0530

    Simple Question

---
+++ Practice1.js
@@ -0,0 +1,11 @@
+
+// Write a JavaScript function to check whether an 'input' is blank or not
+
+function is_Blank(inputabc){
+    if(inputabc.length === 0) return true;
+    else return false;
+}
+
+console.log(is_Blank(""));
+console.log(is_Blank("sncbdcbhdrbc"));
+
